8712915cf93293843d06420cbba9855ff06d4d60
Helge-Frank Zimpel <info@kontainer.sh>
2023-02-24 20:24:38 +0100

    Spring Boot sample

---
+++ sample-app/src/main/java/sh/kontainer/sampleapp/SampleAppApplication.java
@@ -0,0 +1,13 @@
+package sh.kontainer.sampleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleAppApplication.class, args);
+	}
+
+}
